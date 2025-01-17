4e1bb8a570a0e2b94c15bf213c88aa981836cd94
Reda Lakhledj <R.LAKHLEDJ@lprs.fr>
2025-01-17 08:54:32 +0100

    fix(all) les docs sont tous lier

--- Index Cinema/connexion.php
+++ Index Cinema/connexion.php
@@ -19,16 +19,16 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#page-top">CINEMAX - Haut de page</a>
+        <a class="navbar-brand" href="index.html">CINEMAX - Accueil </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="#about">Connexion</a></li>
-                <li class="nav-item"><a class="nav-link" href="#projects">Inscription</a></li>
-                <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
+
+                <li class="nav-item"><a class="nav-link" href="Inscription.php">Inscription</a></li>
+
             </ul>
         </div>
     </div>
